3b62db7360a2157b9c712f1f419618d6bd7b1384
Safwan Diwan <diwan005@umn.edu>
2022-12-07 10:40:13 -0600

    Added another bad algorithm and consolidated code

--- strategies.py
+++ strategies.py
@@ -6,6 +6,9 @@ WORDLEN = 5 # The length of the correct answer and guessed words
 INITGUESS = "TARES" # The first guess
 
 stateSpace = {} # Global value to demonstrate the state space
+letterFreq = {} # Global value to count letter frequencies
+
+rankingType = 0
 
 def resetStateSpace(): # Function to reset state space. Is run after every game play. Keys are letters, and
                             # values are a list where the first value is if the letter was previously guessed.
@@ -15,6 +18,16 @@ def resetStateSpace(): # Function to reset state space. Is run after every game 
     "i" : [0, 1, 1, 1, 1, 1], "j" : [0, 1, 1, 1, 1, 1], "k" : [0, 1, 1, 1, 1, 1], "l" : [0, 1, 1, 1, 1, 1], "m" : [0, 1, 1, 1, 1, 1], "n" : [0, 1, 1, 1, 1, 1], "o" : [0, 1, 1, 1, 1, 1], "p" : [0, 1, 1, 1, 1, 1],
     "q" : [0, 1, 1, 1, 1, 1], "r" : [0, 1, 1, 1, 1, 1], "s" : [0, 1, 1, 1, 1, 1], "t" : [0, 1, 1, 1, 1, 1], "u" : [0, 1, 1, 1, 1, 1], "v" : [0, 1, 1, 1, 1, 1], "w" : [0, 1, 1, 1, 1, 1], "x" : [0, 1, 1, 1, 1, 1],
     "y" : [0, 1, 1, 1, 1, 1], "z" : [0, 1, 1, 1, 1, 1]
+}
+
+def resetLetterFreq(): # Function to reset letter frequency count. Is run after every game play. Keys are letters, and
+                            # frequency values are a list.
+    global letterFreq
+    letterFreq = {
+    "a" : [0, 0, 0, 0, 0], "b" : [0, 0, 0, 0, 0], "c" : [0, 0, 0, 0, 0], "d" : [0, 0, 0, 0, 0], "e" : [0, 0, 0, 0, 0], "f" : [0, 0, 0, 0, 0], "g" : [0, 0, 0, 0, 0], "h" : [0, 0, 0, 0, 0],
+    "i" : [0, 0, 0, 0, 0], "j" : [0, 0, 0, 0, 0], "k" : [0, 0, 0, 0, 0], "l" : [0, 0, 0, 0, 0], "m" : [0, 0, 0, 0, 0], "n" : [0, 0, 0, 0, 0], "o" : [0, 0, 0, 0, 0], "p" : [0, 0, 0, 0, 0],
+    "q" : [0, 0, 0, 0, 0], "r" : [0, 0, 0, 0, 0], "s" : [0, 0, 0, 0, 0], "t" : [0, 0, 0, 0, 0], "u" : [0, 0, 0, 0, 0], "v" : [0, 0, 0, 0, 0], "w" : [0, 0, 0, 0, 0], "x" : [0, 0, 0, 0, 0],
+    "y" : [0, 0, 0, 0, 0], "z" : [0, 0, 0, 0, 0]
 }
 
 def updateStateSpace(guess, result): # Update state space as a result of each guess.
@@ -97,9 +110,19 @@ def playGame(wordGen):
     """)
     return NUM_GUESSES
 
-def updateRanking(wordDict): # Update ranking (0 if letter not in word, 1 if in word but in incorrect position, 2 if in word and correct position)
+def updateRanking(wordDict): # Function to call the correct ranking function
+    global rankingType
+    if (rankingType == 0):
+        return wordDict
+    if (rankingType == 1):
+        return updateStateSpaceRanking(wordDict)
+    if (rankingType == 2):
+        return updateFrequencyRanking(wordDict)
+
+def updateStateSpaceRanking(wordDict): # Update ranking (0 if letter not in word, 1 if in word but in incorrect position, 2 if in word and correct position)
     global stateSpace
     for word in wordDict:
+        print(word)
         ranking = 0
         position = 1
         for letter in word:
@@ -109,7 +132,31 @@ def updateRanking(wordDict): # Update ranking (0 if letter not in word, 1 if in 
         wordDict.update({word : ranking})
     return wordDict
 
-def isValid(word):
+def updateFrequencyRanking(wordDict):
+    global letterFreq
+    resetLetterFreq()
+    for word in wordDict: # Updates the letterFreq dictionary with updated frequency values
+        position = 0
+        for letter in word:
+            letter = letter.lower()
+            frequencyList = letterFreq.get(letter)
+            dupStr = word.split(letter)
+            if frequencyList != None and not (letter in dupStr):
+                frequency = frequencyList[position] + 1
+                frequencyList[position] = frequency
+                letterFreq.update({letter : frequencyList})
+            position += 1
+    for word in wordDict:
+        ranking = 0
+        position = 0
+        for letter in word:
+            letter = letter.lower()
+            frequencyList = letterFreq.get(letter)
+            ranking += frequencyList[position]
+        wordDict.update({word : ranking})
+    return wordDict
+
+def isValid(word): # Check if word is valid (Wordle hard mode verification)
     global stateSpace
     position = 1
     for letter in word: # Iterate through each letter in the word, check if this is a possible word
@@ -119,9 +166,10 @@ def isValid(word):
         position += 1
     return True
 
-
-def randomGuesser(wordDict, guessNum):
+def randomGuesser(wordDict, guessNum): # Guess random words that are valid based on previous guess info
     global stateSpace
+    global rankingType
+    rankingType = 0
     wordList = list(wordDict.keys()) # Get list of keys (words)
     random.shuffle(wordList) # Shuffle list for random aspect of search
     for word in wordList:
@@ -132,37 +180,50 @@ def randomGuesser(wordDict, guessNum):
         else:
             del wordDict[word.upper()]            
 
-def useLowestScore2and3Guess(wordDict, guessNum):      
+def getHighestRanking(wordDict):
     wordList = sorted(wordDict.items(), key=lambda x:x[1]) # Sort the dictionary (converts it to a list)
     updatedWordList = wordList.copy()
-    if guessNum == 1 or guessNum == 2: # If the second or the third guess...
-        for wordTuple in wordList:
-            updatedWordList.remove(wordTuple) # Remove this word from the list
-            if isValid(wordTuple[0].lower()):
-                return dict(updatedWordList), wordTuple[0] # Cast the sorted list to a dictionary and also return the next guess
-    else: 
-        for wordTuple in reversed(wordList): # Get highest score words
+    for wordTuple in reversed(wordList): # Get highest score words
             updatedWordList.remove(wordTuple)
             if isValid(wordTuple[0].lower()):
                 return dict(updatedWordList), wordTuple[0]
 
-def useAverageScore2and3Guess(wordDict, guessNum):      
+def getAverageRanking(wordDict):
     wordList = sorted(wordDict.items(), key=lambda x:x[1]) # Sort the dictionary (converts it to a list)
-    if guessNum == 1 or guessNum == 2: # If the second or the third guess...
-        while True:
-            word = wordList[int(len(wordList) / 2)][0] # Get the word with the lowest score
+    while True:
+            word = wordList[int(len(wordList) / 2)][0] # Get the word with the average score
             wordList.pop(int(len(wordList) / 2)) # Remove this word from the list
             if isValid(word.lower()):
                 return dict(wordList), word # Cast the sorted list to a dictionary and also return the next guess
-    else: 
-        updatedWordList = wordList.copy()
-        for wordTuple in reversed(wordList): # Get highest score words
-            updatedWordList.remove(wordTuple)
+
+def getLowestRanking(wordDict):
+    wordList = sorted(wordDict.items(), key=lambda x:x[1]) # Sort the dictionary (converts it to a list)
+    updatedWordList = wordList.copy()
+    for wordTuple in wordList:
+            updatedWordList.remove(wordTuple) # Remove this word from the list
             if isValid(wordTuple[0].lower()):
-                return dict(updatedWordList), wordTuple[0]
+                return dict(updatedWordList), wordTuple[0] # Cast the sorted list to a dictionary and also return the next guess
+
+def useLowestScore2and3Guess(wordDict, guessNum): 
+    global rankingType
+    rankingType = 0  
+    if guessNum == 1 or guessNum == 2: # If the second or the third guess...
+        return getLowestRanking(wordDict)
+    else: 
+        return getHighestRanking(wordDict)
+
+def useAverageScore2and3Guess(wordDict, guessNum):      
+    global rankingType
+    rankingType = 0 
+    if guessNum == 1 or guessNum == 2: # If the second or the third guess...
+        return getAverageRanking(wordDict)
+    else: 
+        return getHighestRanking(wordDict)
 
 def useLettersInIncorrectSpots(wordDict, guessNum):
     global stateSpace
+    global rankingType
+    rankingType = 0  
     wordList = list(wordDict.keys()) # Get list of keys (words)
     random.shuffle(wordList) # Shuffle list for random aspect of search
     if guessNum == 1 or guessNum == 2:
@@ -177,28 +238,40 @@ def useLettersInIncorrectSpots(wordDict, guessNum):
                 del wordDict[word.upper()]
                 return wordDict, word
     else:
-        wordList = sorted(wordDict.items(), key=lambda x:x[1]) # Sort the dictionary (converts it to a list)
-        updatedWordList = wordList.copy()
-        for wordTuple in reversed(wordList): # Get highest score words
-            updatedWordList.remove(wordTuple)
-            if isValid(wordTuple[0].lower()):
-                return dict(updatedWordList), wordTuple[0]
-        return
+        return getHighestRanking(wordDict)
+
+def useAverageFrequencyToGuess(wordDict, guessNum):
+    global rankingType
+    rankingType = 2
+    return getAverageRanking(wordDict)
+    # if guessNum == 2 or guessNum == 3:
+    #     return getAverageRanking(wordDict)
+    # else:
+    #     return getHighestRanking(wordDict)
 
 
 def runStrategy (strategy, strategyName, iterations):
     print("Running", strategyName, "Algorithm with", iterations, "game iterations. This may take a while...")
     tries = 0
     startTime = time.perf_counter()
+    underOrEqualTo6 = 0
+    over6 = 0
     for x in range (iterations):
-        tries += playGame(strategy)
+        guesses = playGame(strategy)
+        tries += guesses
+        if guesses >= 6:
+            underOrEqualTo6 += 1
+        else:
+            over6 += 1
     endTime = time.perf_counter()
     totalTime = endTime - startTime
     print("    Average Guesses for "+ str(strategyName) + ": " + str(tries / iterations))
+    print("    Number of Games with guess count under or equal to 6: "+ str(underOrEqualTo6) + ". With guess count over 6: " + str(over6))
     print("    Time taken for algorithm to run: " + str(totalTime) + " secs")
     print("    This means each game took, on average, " + str(totalTime / iterations) + " secs to run")
 
-runStrategy(randomGuesser, "RandomGuesser", 100)
-runStrategy(useLowestScore2and3Guess, "Lowest Score for 2nd & 3rd Guesses", 100)
-runStrategy(useAverageScore2and3Guess, "Average Score for 2nd & 3rd Guesses", 100)
-runStrategy(useLettersInIncorrectSpots, "Guess Words with letters not in correct spot for 2nd & 3rd Guesses", 100)
+# runStrategy(randomGuesser, "RandomGuesser", 100)
+# runStrategy(useLowestScore2and3Guess, "Lowest Score for 2nd & 3rd Guesses", 10)
+# runStrategy(useAverageScore2and3Guess, "Average Score for 2nd & 3rd Guesses", 10)
+# runStrategy(useLettersInIncorrectSpots, "Guess Words with letters not in correct spot for 2nd & 3rd Guesses", 10)
+runStrategy(useAverageFrequencyToGuess, "AverageFrequencyGuesser", 10)
